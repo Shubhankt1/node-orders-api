b26ba65ad1f20ac552afe01ed5b2b8cf0674831d
Shubhank Tyagi <shubhank.tyagi@tipotapp.com>
2024-07-31 12:32:45 -0400

    added body parser added cors

--- app.js
+++ app.js
@@ -11,6 +11,29 @@ const ordersRoutes = require('./api/routes/orders');
 const morgan = require('morgan');
 app.use(morgan('dev'));
 
+const bodyParser = require('body-parser');
+// To parse URLEncoded bodies
+app.use(bodyParser.urlencoded({ extended: true }));	// Parse extended bodies with rich data
+// To parse json
+app.use(bodyParser.json());
+
+// CORS
+app.use((req, res, next) => {
+	res.header('Access-Control-Allow-Origin', '*');
+	res.header(
+		'Access-Control-Allow-Headers',
+		'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+	);
+	if (req.method === 'OPTIONS') {
+		res.header(
+			'Access-Control-Allow-Methods',
+			'GET, POST, PUT, PATCH, DELETE'
+		);
+		return res.status(200).json({});
+	}
+	next();
+});
+
 // Routes
 app.use('/products', productsRoutes);
 app.use('/orders', ordersRoutes);
